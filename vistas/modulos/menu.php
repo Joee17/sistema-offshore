a7da8388675d48aeb581068aa24814438e51bb21
Joee17 <MPAREDESJOELHAR@crece.uss.edu.pe>
2023-09-08 13:08:37 -0500

    Se agrega el Proyecto

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,100 @@
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+			<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="usuarios">
+
+					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
+
+				</a>
+
+			</li>
+			<li class="treeview">
+
+				<a href="#">
+
+					<i class="fa fa-list-ul"></i>
+					
+					<span>Mantenimiento</span>
+					
+					<span class="pull-right-container">
+					
+						<i class="fa fa-angle-left pull-right"></i>
+
+					</span>
+
+				</a>
+
+				<ul class="treeview-menu">
+					
+					<li>
+
+						<a href="empleado">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Administrar empleados</span>
+
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="proyectos">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Proyectos</span>
+
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="permisos">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Permisos</span>
+
+						</a>
+
+					</li>
+
+				</ul>
+
+				<li>
+
+				<a href="consultas">
+
+					<i class="fa fa-users"></i>
+					<span>Consultas</span>
+
+				</a>
+
+			</li>
+
+			</li>
+
+		</ul>
+
+		
+
+	 </section>
+
+</aside>
